5128ca52a2f53f85ee498955ec9bfa3f49cdfc85
quantvph50908 <quantvph50908@gmail.com>
2025-07-06 07:56:57 +0700

    Add Sound

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundClip;
+    public AudioClip LoseClip;
+    public AudioClip WinClip;
+    public AudioClip ClickClip;
+
+    [Header("Audio Sources")]
+    private AudioSource backgroundSource;
+    private AudioSource effectSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+       
+        backgroundSource = gameObject.AddComponent<AudioSource>();
+        effectSource = gameObject.AddComponent<AudioSource>();
+
+        
+        if (backgroundClip != null)
+        {
+            backgroundSource.clip = backgroundClip;
+            backgroundSource.loop = true;
+            backgroundSource.Play();
+        }
+    }
+
+    public void Lose()
+    {
+        if (LoseClip != null && effectSource != null)
+        {
+            effectSource.PlayOneShot(LoseClip);
+        }
+    }
+
+    public void Win()
+    {
+        if (WinClip != null && effectSource != null)
+        {
+            effectSource.PlayOneShot(WinClip);
+        }
+    }
+
+    public void Click()
+    {
+        if (ClickClip != null)
+        {
+            effectSource.PlayOneShot(ClickClip);
+        }
+    }
+
+}
